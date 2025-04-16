0c2c108fd7cb05248c9a54ca58059a62fef0bb39
kyliehuch <kylie.huch@gmail.com>
2025-04-16 16:12:24 -0700

    Connectivity analysis of EPG

--- lib.py
+++ lib.py
@@ -36,6 +36,8 @@ def skeleton_synapse_visualization(body_Id, type_pre=None, type_post=None, rois_
         * batch_size (int): batch size for fetch_synapse_connections call
             - Decrease this number if you experience timeouts while running this function
     """
+    # TODO: add support for all pre/post types (optionally color by type or instance)
+    # TODO: add support for instances (color by bodyId)
     # TODO: add synapse_size variable
     # TODO: add dim variable 
     # TODO: add batch_size support
@@ -53,6 +55,136 @@ def skeleton_synapse_visualization(body_Id, type_pre=None, type_post=None, rois_
     post_top_conns = None
     if type_pre:
         upstream_cri = neuprint.NeuronCriteria(type=type_pre+'.*') if type_pre else None
+        pre_syn_cri = neuprint.SynapseCriteria(rois=rois_pre, primary_only=primary_only)
+        print("Fetching pre-synaptic connections...")
+        pre_conn_df = neuprint.fetch_synapse_connections(upstream_cri, neuron_cri, pre_syn_cri)
+        pre_neurons, _ = neuprint.fetch_neurons(pre_conn_df['bodyId_pre'].unique())
+        pre_conn_df = neuprint.utils.merge_neuron_properties(pre_neurons, pre_conn_df, 'instance')
+        if top: 
+            pre_top_conns = pre_conn_df['instance_pre'].value_counts().head(top)
+        else:
+            pre_top_conns = pre_conn_df['instance_pre'].value_counts()
+        if not pre_palett:
+            lc = len(pre_top_conns)
+            if lc <= 11:
+                pre_palett = bokeh.palettes.Plasma[lc if lc>2 else 3]
+            elif lc > 100:
+                pre_palett = bokeh.palettes.Plasma256
+            else:
+                if loop_colors:
+                    pre_palett = bokeh.palettes.Plasma11
+                else:
+                    pre_palett = bokeh.palettes.Iridescent23
+        pre_points = pre_conn_df.query('instance_pre in @pre_top_conns.index').copy()
+        pre_colors = (pre_palett * (len(pre_points) // len(pre_palett) + 1))[:len(pre_points)]
+        pre_points['color'] = pre_points['instance_pre'].map(dict(zip(pre_top_conns.index, pre_colors)))
+        p.scatter(pre_points['x_pre'], pre_points['z_pre'], color=pre_points['color'])
+    if type_post:
+        downstream_cri = neuprint.NeuronCriteria(type=type_post+'.*') if type_post else None
+        post_syn_cri = neuprint.SynapseCriteria(rois=rois_post, primary_only=primary_only)
+        print("Fetching post-synaptic connections...")
+        post_conn_df = neuprint.fetch_synapse_connections(neuron_cri, downstream_cri, post_syn_cri)
+        post_neurons, _ = neuprint.fetch_neurons(post_conn_df['bodyId_post'].unique())
+        post_conn_df = neuprint.utils.merge_neuron_properties(post_neurons, post_conn_df, 'instance')
+        if top:
+            post_top_conns = post_conn_df['instance_post'].value_counts().head(top)
+        else:
+            post_top_conns = post_conn_df['instance_post'].value_counts()
+        if not post_palett:
+            lc = len(post_top_conns)
+            if lc <= 11:
+                post_palett = bokeh.palettes.Viridis[lc if lc>2 else 3]
+            elif lc > 100:
+                post_palett = bokeh.palettes.Viridis256
+            else:
+                if loop_colors:
+                    post_palett = bokeh.palettes.Viridis11
+                else:
+                    post_palett = bokeh.palettes.Viridis256
+        post_points = post_conn_df.query('instance_post in @post_top_conns.index').copy()
+        post_colors = (post_palett * (len(post_points) // len(post_palett) + 1))[:len(post_points)]
+        post_points['color'] = post_points['instance_post'].map(dict(zip(post_top_conns.index, post_colors)))
+        p.scatter(post_points['x_post'], post_points['z_post'], color=post_points['color'])
+    show(p)
+
+    return pre_top_conns, post_top_conns
+
+
+# maybe split into 2 functions: fetch synapses and pliot skeleton
+def skeleton_synapse_visualization_flex(body_Id, pre_scale=None, post_scale=None, pre_id=None, post_id=None, pre_lable_res=None, post_lable_res=None, rois_pre=None, rois_post=None, top=None, primary_only=True, skeleton_color=bokeh.palettes.Inferno3[0], pre_palett=None, post_palett=None, loop_colors=True, dim=2, synapse_size=None, batch_size=None):
+    """ Function returning a graphic of the skeleton of neuron specified by body_Id with the desired synapses plotted colored by pre/post synpase and neuron subtype
+        * body_Id (int): body ID of the given neuron would you like to examine
+        * pre_scale (str): scale at which to examine synapses onto the given neuron from
+            - 'instance': anylize synapses onto the specified neuron from a given presynaptic instance (subtype)
+                * NOTE: must specify neuprint neuron instance (subtype) name as 'pre_id' argument
+            - 'type': anylize synapses onto the specified neuron from given presynaptic type
+                 * NOTE: must specify neuprint neuron type name as 'pre_id' argument
+            - 'all': anylize synapses onto the specified neuron from all presynaptic neurons
+            - None: don't analyze pre synaptic connections
+        * post_scale (str): scale at which to examine synapses from the given neuron onto
+            - 'instance': anylize synapses from the specified neuron onto a given postsynaptic instance (subtype)
+                * NOTE: must specify neuprint neuron instance (subtype) name as 'post_id' argument
+            - 'type': anylize synapses from the specified neuron onto given postsynaptic type
+                 * NOTE: must specify neuprint neuron type name as 'post_id' argument
+            - 'all': anylize synapses from the specified neuron onto all postsynaptic neurons
+            - None: don't analyze post synaptic connections
+        * pre_id (str): neuprint identifier for presynaptic neuron instance/type
+        * post_id (str): neuprint identifier for postsynaptic neuron instance/type
+        * pre_lable_res (str): resolution at which to label presynapses
+            - 'neuron': must have a minimum pre_scale of 'instance'
+            - 'instance': must have a minimum pre_scale of 'type'
+            - 'type': must have a minimum pre_scale of 'all'
+        * post_lable_res (str): resolution at which to label postsynapses
+            - 'neuron': must have a minimum post_scale of 'instance'
+            - 'instance': must have a minimum post_scale of 'type'
+            - 'type': must have a minimum post_scale of 'all'
+        * rois_pre (list of str): return only pre-synaptic connections within the given ROIs, leave as None if interested in all synapses
+        * rois_post (list of str): return only post-synaptic connections within the given ROIs, leave as None if interested in all synapses
+        * top (int): number of neurons to visualize connections from/to
+            - If left as None will return synapses from/to all neurons matching the query, otherwise returns synapses from/to specified number of neurons sorted by highest number of synapses
+        * primary_only (bool): return only primary synapses of the given type
+        * skeleton_color (bokeh palett string): desired color descriptor for skeleton (default: black)
+        * pre_palett (bokeh palett object): palett to use for pre-synaptic connections
+        * post_palett (bokeh palett object): palett to use for post-synaptic connections
+        * loop_colors (bool): flag for weather or not to repeat colors over multiple neurons 
+            - TIP: if you are plotting connections from less than ~100 neurons you should leave this as 'True' or the difference bewteen synapses of different neurons will be very hard to visually distingish 
+            - NOTE: The maximum number of colors avaiable in the palettes is 256 so if you are plotting connections to more neurons than the colors will be repeated regardless of how you set this flag
+        * dim ([2,3]): dimensionality of the rendering (pick 2 or 3)
+        * synapse_size (int): controls size of plotted synapses on the skeleton
+        * batch_size (int): batch size for fetch_synapse_connections call
+            - Decrease this number if you experience timeouts while running this function
+    """
+    # TODO: add support for all pre/post types (optionally color by type or instance)
+    # TODO: add support for instances (color by bodyId)
+    # TODO: add support for lists of pre/post synaptic neuron instances/types 
+    # TODO: add synapse_size variable
+    # TODO: add dim variable 
+    # TODO: add batch_size support
+    assert pre_scale or post_scale, "Error: must specify either pre or post synaptic neuron scale"
+    if pre_scale: assert pre_scale in ['instnace', 'post', 'all'], "Error: must specify presynaptic scale of 'instance', 'type', or 'all'"
+    if post_scale: assert post_scale in ['instnace', 'post', 'all'], "Error: must specify postsynaptic scale of 'instance', 'type', or 'all'"
+    neuron_cri = neuprint.NeuronCriteria(bodyId=body_Id)
+    p = figure()
+    p.y_range.flipped = True
+    s = neuprint.skeleton.fetch_skeleton(body_Id, format='pandas')
+    s['bodyId'] = body_Id
+    s['color'] = skeleton_color
+    s = s.merge(s, 'inner', left_on=['bodyId', 'link'], right_on=['bodyId', 'rowId'], suffixes=['_child', '_parent'])
+    p.segment(x0='x_child', x1='x_parent', y0='z_child', y1='z_parent', color='color_child', source=s)
+    
+    pre_top_conns = None
+    post_top_conns = None
+    if pre_scale:
+        if pre_scale=='instance':
+            upstream_cri = neuprint.NeuronCriteria(instance=pre_id)
+            if pre_lable_res==None: pre_lable_res = 'bodyId'
+        elif pre_scale=='type':
+            upstream_cri = neuprint.NeuronCriteria(type=pre_id)
+            if pre_lable_res==None: pre_lable_res = 'instance'
+        else:
+            upstream_cri = None
+            if pre_lable_res==None: pre_lable_res = 'type'
+        if pre_lable_res=='neuron': pre_lable_res = 'bodyId'
         pre_syn_cri = neuprint.SynapseCriteria(rois=rois_pre, primary_only=primary_only)
         print("Fetching pre-synaptic connections...")
         pre_conn_df = neuprint.fetch_synapse_connections(upstream_cri, neuron_cri, pre_syn_cri)
@@ -170,6 +302,7 @@ def fetch_connectivity(target_scale, conn_scale, conn_type, target_id, conn_id=N
     conns.sort_values('weight', ascending=False, inplace=True)
     # manually remove any 'NotPrimary' synapses (even with include_nonprimary=False some are included!)
     conns = conns[conns['roi']!='NotPrimary']
+    conns = conns[['bodyId_pre', 'instance_pre', 'type_pre', 'bodyId_post', 'instance_post', 'type_post', 'roi', 'weight']]
     return conns
 
 
